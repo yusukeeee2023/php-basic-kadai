ae4a5c3d5a9162b7ad9fa929c71361cf267924d3
yusukeeee2023 <yusukeeee2023@gmail.com>
2024-10-14 10:48:32 +0900

    Add files via upload

---
+++ kadai_007/associative-array.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+ <html lang="ja">
+ 
+ <head>
+     <meta charset="UTF-8">
+     <title>PHP基礎編</title>
+ </head>
+ 
+ <body>
+     <p>
+         <?php
+         $personal_value = ['name' => 'onion' , 'price' => '200' , 'weight' => '160'];
+         print_r($personal_value)
+         ?>
+     </p>
+ </body>
+ 
+ </html>
